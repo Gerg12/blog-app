d029277813f1fa4930491a3212210d2deddf06e1
Gregory Johnson <gregjohnson@Gregorys-iMac.local>
2023-12-01 13:47:44 -0500

    [~] About page setup.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { fetchPageContent } from '../../utils/api';
 
 const About = () => {
   const [pageContent, setPageContent] = useState('');
   const [pageTitle, setPageTitle] = useState('');
 
   useEffect(() => {
-    fetch('https://js1.10up.com/wp-json/wp/v2/pages')
-      .then(response => response.json())
+    const pageId = 91;
+    fetchPageContent(pageId)
       .then(data => {
-        const aboutPage = data.find(page => page.id === 91);
-        setPageContent(aboutPage?.content?.rendered || '');
-        setPageTitle(aboutPage?.title?.rendered || '');
-      })
-      .catch(error => {
-        console.error('Error fetching page content:', error);
+        setPageContent(data?.content?.rendered || '');
+        setPageTitle(data?.title?.rendered || '');
       });
   }, []);
 
