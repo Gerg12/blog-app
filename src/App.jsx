bf7cd529fc435f4812d0d1546d7c2462d9e864da
Gregory Johnson <gregjohnson@Gregorys-iMac.local>
2023-12-01 11:09:14 -0500

    [~] remove old import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
