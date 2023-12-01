206298d6f5b371550a2efbe59afff392e11761f2
Gregory Johnson <gregjohnson@Gregorys-iMac.local>
2023-12-01 11:41:16 -0500

    [~] Add about page content.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const About = () => {
+  const [pageContent, setPageContent] = useState('');
+
+  useEffect(() => {
+    fetch('https://js1.10up.com/wp-json/wp/v2/pages')
+      .then(response => response.json())
+      .then(data => {
+        // Assuming the about page has an ID of 1
+        const aboutPage = data.find(page => page.id === 91);
+        setPageContent(aboutPage?.content?.rendered || '');
+      })
+      .catch(error => {
+        console.error('Error fetching page content:', error);
+      });
+  }, []);
+
   return (
     <div className="about">
-      <h1>
-					About
-			</h1>
-      <div className="page">
-				10up about text...
-      </div>
+      <h1>About</h1>
+      <div className="page" dangerouslySetInnerHTML={{ __html: pageContent }} />
     </div>
   );
 };
